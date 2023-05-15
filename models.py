adebdd49bb847cc406a1b8cb71290776099916c5
Yussef raouf <yussef.raouf11@gmail.com>
2023-05-15 11:57:29 +0300

    create user table

---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from typing import Optional, List
+from uuid import UUID, uuid4
+from enum import Enum
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    sudent = "student"
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    first_name : str
+    last_name : str
+    middle_name : Optional[str]
+    gender: Gender
+    roles: List[Role]
